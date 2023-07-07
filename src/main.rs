5d7429bb70f89f54c1a60cd3d55b5bc41387a892
Guilherme Leite <guilhermecesarleite@gmail.com>
2023-07-06 22:15:21 -0300

    cleanup and restart

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 extern crate windows;
 use windows::{
-    core::*,
-    Win32::Foundation::*,
+    // core::*,
+    // Win32::Foundation::*,
     Win32::System::Threading::*,
-    Win32::UI::WindowsAndMessaging::*,
+    // Win32::UI::WindowsAndMessaging::*,
     Win32::System::Memory::*,
-    windows::Win32::System::WindowsProgramming::INFINITE,
-    MemoryProtection
+    Win32::System::WindowsProgramming::INFINITE,
 };
-use std::ptr::*;
-use std::mem::MaybeUninit;
-use std::time::{Instant, Duration};
+// use std::ptr::*;
+// use std::mem::MaybeUninit;
+// use std::time::{Instant, Duration};
+use std::time::{Instant};
 
 
-unsafe extern "system" fn callback(_lpFlsData: *const std::ffi::c_void) {
+unsafe extern "system" fn callback(_lp_fls_data: *const std::ffi::c_void) {
     // Your callback logic here
 }
 
@@ -66,9 +66,10 @@ fn main() {
 
     unsafe { std::ptr::copy_nonoverlapping(buf.as_ptr(), addr as *mut u8,  size) };
 
-    let thread_id = unsafe {
-        extern "system" fn worker_thread(lpThreadParameter: *mut ::std::os::raw::c_void) -> u32 {
+    let _thread_id = unsafe {
+        extern "system" fn worker_thread(lp_thread_parameter: *mut ::std::os::raw::c_void) -> u32 {
             println!("Thread started!");
+            println!("Thread parameter: {:?}", lp_thread_parameter);
             0
         }
 
@@ -85,14 +86,15 @@ fn main() {
             return;
         }
 
-        WaitForSingleObject(t_handle, INFINITE);
+        WaitForSingleObject(t_handle.unwrap(), INFINITE);
     };
+    // println!("{}", de_xor(&mut buf).len() )
 }
 
-fn de_xor(input: &mut [u8]) -> &[u8] {
-    let encrytor: u8 = 0x77;
-    for i in 0..input.len() {
-        input[i] ^= encrytor;
-    }
-    input
-}
+// fn de_xor(input: &mut [u8]) -> &[u8] {
+//     let encrytor: u8 = 0x77;
+//     for i in 0..input.len() {
+//         input[i] ^= encrytor;
+//     }
+//     input
+// }
